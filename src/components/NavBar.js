1d2e36eaa08d2d69ff38109523381cad68caa789
German Burgardt <edusierra94@gmail.com>
2022-08-15 13:13:32 -0300

    fixNavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,8 +8,6 @@ function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <image src= "https://www.google.com/search?q=logo+skate&rlz=1C1ALOY_esAR954AR954&oq=logo+skate&aqs=chrome.0.0i512l4j0i22i30j0i10i22i30j0i22i30i457j0i22i30l3.1082j0j7&sourceid=chrome&ie=UTF-8#imgrc=k402pnt47nwxLM"></image>
-        <Navbar.Brand href="#home">SkateStore</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
